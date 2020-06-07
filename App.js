b16505f4f79e1b6d91153b7229e2f974a088c52c
kb3068 <58498630+kb3068@users.noreply.github.com>
2020-06-06 20:27:24 -0700

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Adding test message, now going to attempt to merge with master. now fetching</Text>
+      <Text>test message to see if team members can edit</Text>
     </View>
   );
 }
