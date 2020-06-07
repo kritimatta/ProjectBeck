e6ad09dfa1a611dabcfcbaf496709ed045e3f9b8
kb3068 <therealkb54@gmail.com>
2020-06-06 20:51:39 -0700

    Adding test message

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>testuser1</Text>
+      <Text>testuser1. adding a test message</Text>
     </View>
   );
 }
